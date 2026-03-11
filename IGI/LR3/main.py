8c6626cefe2c44c62ec43a61c79bc2540746b7f9
KATTSS <katsyarynnks@gmail.com>
2026-03-11 21:35:03 +0300

    Feature: task 2 realisation

--- IGI/LR3/main.py
+++ IGI/LR3/main.py
@@ -20,8 +20,30 @@ def task_1():
 
     print("Enter eps:")
     eps=inp.get_user_float()
-    
+
     results=mth.row_calcilator(x, eps)
     out.print_table(**{'x':x, 'n':results[2], 'F(x)':results[0], 'Math F(x)':results[1], 'eps':eps})
 
-task_1()
+
+# task_1()
+
+def task_2():
+    """Performs main logic for task 2"""
+    out.print_task_annotation(2, "Calculate sum for a numeric sequence," \
+    " count amount of elements < 10." \
+    "\n\tStops after entering '100'")
+
+    sequence_sum=0
+    amount_smaller_10=0
+
+    while True:
+        x=inp.get_user_int()
+        if x==100:
+            print(f"Sum = {sequence_sum}")
+            print(f"Amount of sequence elements smaller than 10: {amount_smaller_10}")
+            return
+        elif x<10:
+            amount_smaller_10 +=1
+        sequence_sum +=x
+
+# task_2()
